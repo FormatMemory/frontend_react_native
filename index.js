7d85f5239baadf67c7b688f0e6ee67a72dcc268f
Yusheng Ding <davidthinkleding@Gmail.com>
2019-04-24 17:42:38 -0700

    add package-lock.json into .gitignore

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import { KeepAwake, registerRootComponent } from 'expo';
 import App from 'App';
+import allReducers from './src/store/reducers/index.js';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 if (__DEV__) {
   KeepAwake.activate();
 }
+
+const store = createStore(allReducers);
 
 registerRootComponent(App);
