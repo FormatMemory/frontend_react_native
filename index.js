c1d8a9de08cded8a758bc8d9f73c497b911db6cf
Yusheng Ding <davidthinkleding@Gmail.com>
2019-04-24 18:17:08 -0700

    apply thunk as middleware with store

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import { KeepAwake, registerRootComponent } from 'expo';
 import App from 'App';
 import allReducers from './src/store/reducers/index.js';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk'
 
 if (__DEV__) {
   KeepAwake.activate();
 }
 
-const store = createStore(allReducers);
+const store = createStore(allReducers, applyMiddleware(thunk));
 
 class App extends React.Component{
     render() {
