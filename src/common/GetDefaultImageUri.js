75c4a400db1bb9d980b8d682df2b9ec7e6d0fd8d
Yusheng Ding <davidthinkleding@Gmail.com>
2019-05-22 17:29:05 -0700

    add getDefaultImageUri to handle image link issue

--- src/common/GetDefaultImageUri.js
+++ src/common/GetDefaultImageUri.js
@@ -1,10 +1,17 @@
-const getDefaultImageUri = (imageLink, defaultLink) => {
-    if(imageLink){
-      return {uri: imageLink}
+import { POSTS_API_ENDOINT } from '../service/config';
+
+const defailtImage = require('../../assets/images/image_not_found.png');
+const defaultProfile = require('../../assets/images/default_profile.png');
+
+export const getDefaultImageUri = (imageLink, isProfile = false) => {
+    if(imageLink && !imageLink.startsWith(POSTS_API_ENDOINT)){
+        return {uri: POSTS_API_ENDOINT+imageLink}
+    }
+    else if(imageLink){
+        return {uri: imageLink};
+    }else if(isProfile){
+        return defaultProfile;
     }else{
-        console.log({uri: defaultLink});
-      return {uri: defaultLink}
+        return defailtImage;
     }
 };
-
-export default getDefaultImageUri;
