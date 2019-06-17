a0f4c2d2111ca581f450af0ef4c58a41c7d870b2
Yusheng Ding <davidthinkleding@Gmail.com>
2019-06-17 10:53:30 -0700

    change api point name

--- src/service/GetDefaultImageUri.js
+++ src/service/GetDefaultImageUri.js
@@ -1,11 +1,11 @@
-import { POSTS_API_ENDOINT } from './config';
+import { API_ENDOINT } from './config';
 
 const defailtImage = require('../../assets/images/image_not_found.png');
 const defaultProfile = require('../../assets/images/default_profile.png');
 
 export const getDefaultImageUri = (imageLink, isProfile = false) => {
-    if(imageLink && !imageLink.startsWith(POSTS_API_ENDOINT)){
-        return {uri: POSTS_API_ENDOINT+imageLink}
+    if(imageLink && !imageLink.startsWith(API_ENDOINT)){
+        return {uri: API_ENDOINT+imageLink}
     }
     else if(imageLink){
         return {uri: imageLink};
